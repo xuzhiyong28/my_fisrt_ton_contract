6aa56317aa4dddb20774a7f5bfa30261e66243ab
xuzy <jip0574@qq.com>
2025-06-02 21:26:06 +0800

    提交

--- first_contract_front_end/src/main.tsx
+++ first_contract_front_end/src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import {TonConnectUIProvider} from "@tonconnect/ui-react";
+const manifestUrl = "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+        <App/>
+    </TonConnectUIProvider>,
 )
