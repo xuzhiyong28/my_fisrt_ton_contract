f3f775e4b02c8031739480fbd41ca8ce0cb630e8
xuzy <jip0574@qq.com>
2025-06-03 09:46:46 +0800

    提交

--- first_contract_front_end/src/main.tsx
+++ first_contract_front_end/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {TonConnectUIProvider} from "@tonconnect/ui-react";
-const manifestUrl = "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+const manifestUrl = "https://raw.githubusercontent.com/xuzhiyong28/my_fisrt_ton_contract/refs/heads/master/first_contract_front_end/tonconnect-manifest.json";
 createRoot(document.getElementById('root')!).render(
     <TonConnectUIProvider manifestUrl={manifestUrl}>
         <App/>
