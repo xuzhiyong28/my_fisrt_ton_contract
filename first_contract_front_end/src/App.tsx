c551be5bcaf00dd94878399db46513f818613f44
xuzy <jip0574@qq.com>
2025-06-08 21:48:55 +0800

    提交

--- first_contract_front_end/src/App.tsx
+++ first_contract_front_end/src/App.tsx
@@ -3,13 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {TonConnectButton} from "@tonconnect/ui-react"
+import {useMainContract} from "./hooks/useMainContract";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const [
+      contract_address,
+      counter_value,
+      recent_sender,
+      owner_address,
+      contract_balance
+  ] = useMainContract();
   return (
     <div className='App'>
         <TonConnectButton />
     </div>
+
   )
 }
 
--- first_contract_front_end/src/hooks/useMainContract.ts
+++ first_contract_front_end/src/hooks/useMainContract.ts
@@ -3,3 +3,45 @@ import {MainContract} from "../contracts/MainContract";
 import {useTonClient} from "./useTonClient";
 import {useAsyncInitialize} from "./useAsyncInitialize";
 import {Address, OpenedContract} from "ton-core";
+import {b} from "vite/dist/node/moduleRunnerTransport.d-DJ_mE5sf";
+
+export function useMainContract() {
+    const client = useTonClient();
+    const [contractData, setContractData] = useState<null | {
+        counter_value: number;
+        recent_sender: Address;
+        owner_address: Address;
+    }>();
+    const [balance, setBalance] = useState<null | number>(0);
+
+    const mainContract = useAsyncInitialize(async () => {
+        if(!client) return;
+        const contract = new MainContract(
+            Address.parse("EQB8uY1gmF6z0h4I3sAFk0CP2LWRrwewnCbn7EYIaQ77y7XR")
+        );
+        return client.open(contract) as OpenedContract<MainContract>;
+    }, [client]);
+
+    useEffect(() => {
+        async function getValue(){
+            if(!mainContract) return;
+            setContractData(null);
+            const val = await mainContract.getData();
+            const balance = await mainContract.getBalance();
+            setContractData({
+                counter_value: val.number,
+                recent_sender: val.recent_sender,
+                owner_address: val.owner_address
+            });
+            setBalance(balance);
+        }
+        getValue();
+    }, [mainContract]);
+    return {
+        contract_address: mainContract?.address.toString(),
+        contract_balance: balance,
+        ...contractData
+    }
+}
+
+
